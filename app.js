0866b82c5813747a11a3062d47a2cac51acbdfb6
qq2828 <282877704@qq.com>
2023-06-06 16:58:00 +0800

    稍微做了些许优化，可以自动检测所有csv打包上传

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 const csvToJson = require('./csvToJson');
 const uploadToMongoDB = require('./uploadToMongoDB');
 
-const csvFilePath = './人力信息.csv';
+const directoryPath = '.';
 const mongoDBUrl = 'mongodb://127.0.0.1:27017/prms?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 
 async function main() {
   try {
-    const jsonArray = await csvToJson(csvFilePath);
-    await uploadToMongoDB(jsonArray, mongoDBUrl);
-    console.log('CSV data successfully uploaded to MongoDB.');
+    fs.readdirSync(directoryPath).forEach(async (file) => {
+      if (path.extname(file) === '.csv') {
+        const jsonArray = await csvToJson(file);
+        await uploadToMongoDB(jsonArray, mongoDBUrl);
+        console.log(`CSV data from ${file} successfully uploaded to MongoDB.`);
+      }
+    });
   } catch (error) {
     console.error('Error:', error);
   }
