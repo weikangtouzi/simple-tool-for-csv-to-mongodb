a487dc5fe557b686cb0c13a61fbd50fbce07e9a8
qq2828 <282877704@qq.com>
2023-06-06 17:05:46 +0800

    优化了转换后数据，去除了对数据库无用的编号字段

--- csvToJson.js
+++ csvToJson.js
@@ -3,7 +3,16 @@ const csv = require('csvtojson');
 async function csvToJson(csvFilePath) {
   try {
     const jsonArray = await csv().fromFile(csvFilePath);
-    return jsonArray;
+    const filteredArray = jsonArray.map(item => {
+      const newItem = { ...item };
+      Object.keys(newItem).forEach(key => {
+        if (key.match(/(序号|编号)/)) {
+          delete newItem[key];
+        }
+      });
+      return newItem;
+    });
+    return filteredArray;
   } catch (error) {
     throw new Error(`Error converting CSV to JSON: ${error}`);
   }
